94fd87c2b5c500da5b91b1e4875fdd00ce8b49d3
Pragya Dwivedi <pragya@dei.ac.in>
2025-06-26 12:01:23 +0530

    Interview CCA Panel User Menu Authority Added

--- src/app/interview-panel/interview-panel.component.ts
+++ src/app/interview-panel/interview-panel.component.ts
@@ -83,7 +83,7 @@ export class InterviewPanelComponent implements OnInit {
         if(res[0].count!==0)
         this.enterIWmarks(val);
         else{
-          alert("Invalid Application Number");
+          alert("You are not authorized for this Application Number");
          this.Appno="";
          return;
 
